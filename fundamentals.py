6e6b31ab96e9c51e6f46ba1f59e70b0cfed12eec
panda1 <blueblue0011@gmail.com>
2022-09-20 18:08:58 -0700

    knowledge check 2 done

--- fundamentals.py
+++ fundamentals.py
@@ -1,6 +1,21 @@
-x = 10
+x = 5
 y = 'hello world'
-this_number = True
+this_number = False
 print(x)
 print(y)
 print(this_number)
+
+if(x > 10):
+    print('this is larger than 10')
+else:
+    print('this is not larger than 10')
+
+if (x < 0 and this_number == True):
+    print('negative and true')
+elif(x > 0 and this_number == False):
+    print('positive and false')
+elif(x > 100 or this_number == True):
+    print('large or true')
+else:
+    print("i don't know")
+
